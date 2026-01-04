e92aa2c9f6fbd89a1c989fbdf2f57013a15d8584
Hiaj <120759779+hiajgbk@users.noreply.github.com>
2026-01-04 12:41:09 +0200

    Update exception.py

--- exception.py
+++ exception.py
@@ -1,3 +1,5 @@
+# Fatma you haven't explain anything here what were you trying to do in with these codes???
+
 def withdraw(balances, amounts):
     if amounts > balances:
         raise ValueError("Insufficient Balance")
@@ -13,4 +15,5 @@ except ValueError as e:
     if "could not convert string to float" in str(e):
         print("Transaction Error: Please enter a valid amount")
     else:
+
         print("Transaction Error:", e)
